563bcbcd5371351e4276f3acc1967d0a0315b1d1
Shanee <shanee@moonactive.com>
2013-08-15 17:37:06 +0300

    shaders

--- cocos2dx/shaders/ccShader_PositionTextureColor_frag.h
+++ cocos2dx/shaders/ccShader_PositionTextureColor_frag.h
@@ -27,14 +27,13 @@
 #ifdef GL_ES								\n\
 precision lowp float;						\n\
 #endif										\n\
-\n\
-//varying vec4 v_fragmentColor;				\n\
+                                            \n\
 varying vec2 v_texCoord;					\n\
 uniform sampler2D CC_Texture0;				\n\
-uniform lowp vec4 u_color;				\n\
-\n\
+uniform lowp vec4 u_color;                  \n\
+                                            \n\
 void main()									\n\
 {											\n\
-gl_FragColor = u_color * texture2D(CC_Texture0, v_texCoord);			\n\
+    gl_FragColor = u_color * texture2D(CC_Texture0, v_texCoord) * u_color.w;			\n\
 }											\n\
 ";
